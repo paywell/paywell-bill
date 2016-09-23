4a385091410705c8d7ef2b3b8cb297904c75f047
lykmapipo <lallyelias87@gmail.com>
2016-09-23 11:30:05 +0300

    update specs

--- spec/index.unit.spec.js
+++ spec/index.unit.spec.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//dependenncies
+const redis = require('paywell-redis')();
+
+describe('bill', function () {
+  before(function (done) {
+    redis.clear(done);
+  });
+
+  describe('create', function () {
+    it('should be able to create bill');
+  });
+
+  describe('get', function () {
+    it('should be able to get bill');
+  });
+
+  describe('search', function () {
+    it('should be able to search bill(s)');
+  });
+
+  after(function (done) {
+    redis.clear(done);
+  });
+
+  after(function () {
+    redis.quit();
+  });
+});
